ee9877811b57f29f60a1692c87d473e79d019b5b
Alexi Reyes <alexireyes6796@gmail.com>
2025-07-11 10:53:07 +0200

    added presets

--- server/handlers/quizPresetHandlers.js
+++ server/handlers/quizPresetHandlers.js
@@ -1,13 +1,11 @@
 import quizzes from '../data/quizzes.js';
 
 export const quizPresetHandlers = (io, socket) => {
-    // Handle request for quiz presets
     socket.on('requestQuizPresets', () => {
         const presetNames = Object.keys(quizzes);
         socket.emit('quizPresetsLoaded', presetNames);
     });
 
-    // Handle request for a specific preset quiz
     socket.on('requestPresetQuiz', (presetName) => {
         const presetQuiz = quizzes[presetName];
         if (presetQuiz) {
