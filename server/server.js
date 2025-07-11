e4bf6dd176e1ea9d50cbdcabf7ec8669f2a4ebdd
Alexi Reyes <alexireyes6796@gmail.com>
2025-07-11 16:33:40 +0200

    revert

--- server/server.js
+++ server/server.js
@@ -3,11 +3,7 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import { registerSocketHandlers } from './handlers/socketHandlers.js';
 import 'dotenv/config';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const server = createServer(app);
@@ -15,7 +11,7 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 registerSocketHandlers(io);
 
